273c10fc0ecb328afe21b8216589080958e51a5e
NathanNavid <naaghajafari@gmail.com>
2023-04-16 19:22:40 -0700

    use more css

--- hook-form/src/Components/From.jsx
+++ hook-form/src/Components/From.jsx
@@ -15,38 +15,38 @@ const From = () => {
 
   return (
         <div className="row">
-            <form action="" className="col-md-4 offset-4">
+            <form action="" className="form col-md-4 offset-4">
                 <div className="form-group">
-                    <label htmlFor="firstName" className="col-form-lable">First Name:</label>
+                    <label htmlFor="firstName" className="initial col-3">First Name:</label>
                     <input type="text" name='firstName' className="form-control" onChange={changeHandler}/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="lastName">Last Name:</label>
+                    <label htmlFor="lastName" className='initial col-3'>Last Name:</label>
                     <input type="text" name='lastName' className="form-control" onChange={changeHandler}/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="email">Email:</label>
+                    <label htmlFor="email" className='initial col-3'>Email:</label>
                     <input type="email" name='email' className="form-control" onChange={changeHandler} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="pasword">Password:</label>
+                    <label htmlFor="pasword" className='initial col-3'>Password:</label>
                     <input type="password" name='password' className="form-control" onChange={changeHandler}/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="confirmPasword">Confirm Password:</label>
+                    <label htmlFor="confirmPasword" className='initial col-3'>Confirm Password:</label>
                     <input type="password" name='confirmPasword' className="form-control" onChange={changeHandler} /><br />
                 </div>
             </form>
-            <div>
+            <div className="view">
                 <h3>Your Form Data </h3>
-                <p>First Name: {user.firstName}</p>
-                <p>Last Name: {user.lastName}</p>
-                <p>Email: {user.email}</p>
-                <p>Password: password</p>
-                <p>Confirm Password: password </p>
+                    <p>First Name: {user.firstName}</p>
+                    <p>Last Name: {user.lastName}</p>
+                    <p>Email: {user.email}</p>
+                    <p>Password: password</p>
+                    <p>Confirm Password: password </p>
             </div>
         </div>
-  )
+)
 }
 
 export default From
